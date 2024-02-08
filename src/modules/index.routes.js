f8fb09ef7382a259206b609b1c7df9b5149c5d8a
MohamedOsamaDev <mohamedosama10085@gmail.com>
2024-02-09 00:49:23 +0200

    n

--- src/modules/index.routes.js
+++ src/modules/index.routes.js
@@ -2,7 +2,6 @@ import { AppError } from "../utils/AppError.js";
 import { dbConnection } from "../../database/dbConnection.js";
 import { globalError } from "./../middleware/global-middleware/globalError.js";
 import { authRouter } from "./auth/auth.routes.js";
-// import { userRouter } from "./user/user.routes.js";
 import { companyRouter } from "./company/company.route.js";
 import { jobRouter } from "./job/job.routes.js";
 import { applicationRouter } from "./application/application.route.js";
@@ -12,7 +11,6 @@ export const bootstrap = (app, express) => {
   app.use(express.json()); // middlewar  for buffer
   app.use("/uploads", express.static("uploads")); // middlewar for File upload
   // start  Endpoints ----------------------------------------- |
-  // app.use(`${mainroute}/users`, userRouter);
   app.use(`${mainroute}/auth`, authRouter);
   app.use(`${mainroute}/companyies`, companyRouter);
   app.use(`${mainroute}/jobs`, jobRouter);
