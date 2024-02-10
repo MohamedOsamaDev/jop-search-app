6b006a1d65f533c5cd979a9b211e8dc5eb217a30
MohamedOsamaDev <mohamedosama10085@gmail.com>
2024-02-10 21:53:44 +0200

    hi

--- src/middleware/authentication/OTPChecker.js
+++ src/middleware/authentication/OTPChecker.js
@@ -1,15 +1,16 @@
 import { AppError } from "../../utils/AppError.js";
 import { AsyncHandler } from "../global-middleware/AsyncHandler.js";
-import  bcrypt  from 'bcrypt';
+import bcrypt from "bcrypt";
 
 export const OTPChecker = AsyncHandler(async (req, res, next) => {
   /*
   read me 
-  this middleware for compare  OTP  before change password
+  this middleware for compare  OTP  before change password and handle password not same as current password
   */
+  // for handle check OTP code
   if (res.locals.user.OTP !== req.body.OTP)
     return next(new AppError("OTP is incorrect", 401));
-
+  // for handle the new password is not the same as the current password
   if (bcrypt.compareSync(req.body.newPassword, res.locals.user.password))
     return next(
       new AppError(
