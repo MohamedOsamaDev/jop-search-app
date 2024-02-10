1ccc170c98a440d6df842c202afa3b7b59fd6553
MohamedOsamaDev <mohamedosama10085@gmail.com>
2024-02-10 20:04:28 +0200

    hi

--- src/middleware/authentication/auth.js
+++ src/middleware/authentication/auth.js
@@ -10,6 +10,25 @@ export const auth = AsyncHandler(async (req, res, next) => {
   */
   // step 1  verify token and check is user authenticated or not
   jwt.verify(req.headers.token, process.env.SECRETKEY, async (err, decoded) => {
+    if (err) return next(new AppError(err, 401));
+
+    const user = await UserModel.findById(decoded?.user);
+
+    if (!user) return next(new AppError("user not found", 401));
+    if (user.passwordChangedAt && decoded.iat * 1000 < new Date(user.passwordChangedAt).getTime())
+      return next(new AppError("token is invaild", 401));
+
+    if (user?.isblocked) return next(new AppError("user is blocked", 401));
+    res.locals.user = user;
+    return next();
+  });
+
+});
+/*
+anthor scenario make same conditions but with more if statement 
+*/
+/*
+  jwt.verify(req.headers.token, process.env.SECRETKEY, async (err, decoded) => {
     // step 2 handle unexpected error like expired token | token is invalid
     if (err) return next(new AppError(err, 401));
     // step 3 find user
@@ -38,24 +57,6 @@ export const auth = AsyncHandler(async (req, res, next) => {
     // step 5 set user in variable
     res.locals.user = user;
     // if all conditions are true will go to next step :)
-    return next();
-  });
-});
-/*
-anthor scenario make same conditions but with more if statement 
-*/
-/*
-  jwt.verify(req.headers.token, process.env.SECRETKEY, async (err, decoded) => {
-    if (err) return next(new AppError(err, 401));
-
-    const user = await UserModel.findById(decoded?.user);
-
-    if (!user) return next(new AppError("user not found", 401));
-    if (user.passwordChangedAt && decoded.iat * 1000 < new Date(user.passwordChangedAt).getTime())
-      return next(new AppError("token is invaild", 401));
-
-    if (user?.isblocked) return next(new AppError("user is blocked", 401));
-    res.locals.user = user;
     return next();
   });
 
